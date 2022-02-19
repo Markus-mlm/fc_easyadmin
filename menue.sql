7aae222f91775ed630c0ad7b015a9e14be98610f
Markus-mlm <m.michalski@bothsides.de>
2022-02-19 23:00:25 +0100

    Updated menue.sql and added short readme.md

--- menue.sql
+++ menue.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: database:3306
--- Erstellungszeit: 19. Feb 2022 um 13:25
--- Server-Version: 10.5.13-MariaDB-1:10.5.13+maria~focal
--- PHP-Version: 7.4.20
+-- Host: localhost
+-- Erstellungszeit: 19. Feb 2022 um 22:45
+-- Server-Version: 10.3.29-MariaDB-0+deb10u1
+-- PHP-Version: 7.3.29-1+0~20210701.86+debian10~1.gbp7ad6eb
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `easy`
+-- Datenbank: `bothsides_easy`
 --
 
 --
@@ -29,12 +30,12 @@ INSERT INTO `menue` (`id`, `name`, `kind`, `price`) VALUES
 (1, 'Mozarella mit Tomate', 'Vorspeise', '5.90'),
 (2, 'Pizza Hawai', 'Hauptspeise', '8.90'),
 (3, 'Hühnersuppe', 'Vorspeise', '4.00'),
-(4, 'Tiramisu', '', '6.90'),
+(4, 'Tiramisu', 'Nachspeise', '6.90'),
 (5, 'Cheeseburger mit Pommes', 'Hauptspeise', '14.60'),
-(6, 'Farmer-Pommes', 'Sidedish', '2.50'),
+(6, 'Farmer-Pommes', 'Beilage', '2.50'),
 (7, 'Brot mit salziger Butter', 'Vorspeise', '5.00'),
 (8, 'New York Cheesecake', 'Nachspeise', '3.40'),
-(9, 'Chickenwings (6 Stk.)', 'Sidedish', '4.90'),
+(9, 'Chickenwings (6 Stk.)', 'Beilage', '4.90'),
 (10, 'Tagiatelle mit Gorgonzolasoße', 'Hauptspeise', '11.50'),
 (11, 'Joghurt mit Honig', 'Nachspeise', '6.70'),
 (12, 'Schnitzel Wienert Art mit Pommes', 'Hauptspeise', '12.90');
---
+++ readme.md
@@ -0,0 +1,17 @@
+#### Requirements
+- PHP 7.4
+- MySQL/MariaDB
+- Apache 2
+
+#### Installation of Playground
+- Download or check out this repository to your DocumentRoot
+- Change database settings in .env
+- Log in to your server, change to DocRoot of server
+- Execute composer install
+- Execute migrations (bin/console d:m:m)
+- Make sure to change DocRoot pointing to public folder or use {Domain}/public
+
+#### Install Easy Admin for Symfony 5
+- composer require easycorp/easyadmin-bundle:^3
+- bin/console make:admin:dashboard
+- Easy Admin is available under {Domain}/admin oder {Domain}/public/admin
